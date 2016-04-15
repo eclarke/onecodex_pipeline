ba5813c1c35c82fc4e8ee24a355fbe3e4fe54fc5
Erik Clarke <ecl@mail.med.upenn.edu>
2016-04-15 17:41:00 -0400

    read from onecodex, get taxa from ncbi, write file

--- get_onecodex_analyses.py
+++ get_onecodex_analyses.py
@@ -1,5 +1,153 @@
-import onecodex
+import os
+import sys
+import time
+import logging
+import itertools
+import functools
+import csv
+
+from urllib.error import HTTPError
+from enum import Enum
+
 import requests
+from Bio import Entrez
+
+Entrez.email = "ecl@mail.med.upenn.edu"
+
+logging.basicConfig(filename="onecodex.log", level=logging.DEBUG, filemode='w')
+stdout_logger = logging.StreamHandler(sys.stdout)
+stdout_logger.setLevel(logging.INFO)
+
+API_KEY = open("ONE_CODEX_API_KEY").read().strip()
+OCX_API = "https://app.onecodex.com/api/v0/"
+
+TaxRanks = [
+    'superkingdom',
+    'phylum',
+    'class',
+    'order',
+    'family',
+    'genus',
+    'species'
+] 
+
+def _get_ocx_url(url, api_key):
+    logging.debug(url)
+    r = requests.get(url, auth=(api_key, ''))
+    r.raise_for_status()
+    return r
 
 
+@functools.lru_cache()
+def get_samples(api_key=API_KEY):
+    """List samples on One Codex."""
+    url = OCX_API + "samples"
+    r = _get_ocx_url(url, api_key)
+    return {s['filename']:s['id'] for s in r.json()}
 
+
+@functools.lru_cache()
+def get_analyses(api_key=API_KEY):
+    """List completed analyses on One Codex."""
+    url = OCX_API + "analyses"
+    return _get_ocx_url(url, api_key).json()
+
+
+def get_analyses_for_id(sample_id, api_key=API_KEY):
+    analyses = get_analyses(api_key)
+    analysis_ids = [_['id'] for _ in analyses if _['sample_id'] == sample_id]
+    for a_id in analysis_ids:
+        url = OCX_API + "analyses/" + a_id
+        yield _get_ocx_url(url, api_key).json()
+
+
+def get_analyses_for_sample(sample_name, api_key=API_KEY):
+    samples = get_samples(api_key)
+    sample_id = samples[sample_name]
+    return get_analyses_for_id(sample_id, api_key)
+
+
+def get_raw_ocx_analysis_for_sample(sample_name, out_fp, api_key=API_KEY):
+    analyses = get_analyses_for_sample(sample_name, api_key)
+    for analysis in analyses:
+        if analysis['reference_name'] == 'One Codex Database':
+            url = OCX_API + "analyses/{}/raw".format(analysis['id'])
+            out = _get_ocx_url(url, api_key)
+            out_filename = os.path.join(out_fp, sample_name + ".ocx.raw.tsv.gz")
+            with open(out_filename, 'wb') as outfile:
+                logging.info("Downloading OCX analysis for {} to {}".format(
+                    sample_name,
+                    out_filename))
+                for chunk in out.iter_content(1024):
+                    outfile.write(chunk)
+            return out_filename
+
+        
+def get_ocx_analysis_for_sample(sample_name, api_key=API_KEY):
+    analyses = get_analyses_for_sample(sample_name, api_key)
+    for analysis in analyses:
+        if analysis['reference_name'] == 'One Codex Database':
+            url = OCX_API + "analyses/{}/table".format(analysis['id'])
+            table = _get_ocx_url(url, api_key).json()
+            return table
+
+        
+def get_taxa_in_sample(sample_name, out_fp, api_key=API_KEY):
+    taxa = get_ocx_analysis_for_sample(sample_name, api_key)
+    tax_ids = [_['tax_id'] for _ in taxa]
+    # Post list to NCBI
+    tax_info = list(itertools.chain.from_iterable(_ncbi_get_many_taxa(tax_ids)))
+    tax_info = [{r['Rank']: r['ScientificName'] for r in t.get('LineageEx', ())} for t in tax_info]
+    print(len(tax_info), len(taxa))
+    assert(len(tax_info) == len(taxa))
+    out_filename = os.path.join(out_fp, sample_name + ".taxa.tsv")
+    with open(out_filename, 'w') as out:
+        writer = csv.DictWriter(
+            out,
+            fieldnames=['tax_id', 'name', 'readcount'] + TaxRanks,
+            delimiter='\t',
+            quoting=csv.QUOTE_MINIMAL,
+            extrasaction='ignore',
+            restval='NA'
+        )
+        writer.writeheader()
+        rows = ({**x, **y} for x, y in zip(taxa, tax_info))
+        writer.writerows(rows)
+    return out_filename
+
+
+def _ncbi_get_many_taxa(ids, batch_size=5000):
+    saved = Entrez.read(Entrez.epost('taxonomy', id=','.join(map(str, ids))))
+    webenv = saved['WebEnv']
+    query_key = saved['QueryKey']
+    for start in range(0, len(ids), batch_size):
+        end = min(len(ids), start + batch_size)
+        attempt = 1
+        while attempt <= 3:
+            try:
+                handle = Entrez.efetch(
+                    db='taxonomy', retstart=start, retmax=batch_size,
+                    webenv=webenv, query_key=query_key)
+                break
+            except HTTPError as err:
+                if 500 <= err.code <= 599:
+                    print("NCBI server error ({}); retrying ({}/3)".format(err.code, attempt))
+                    attempt += 1
+                    time.sleep(15)
+                else:
+                    raise
+        yield(Entrez.read(handle))
+    
+        
+    
+if __name__ == "__main__":
+    log = logging.getLogger(__name__)
+    outlog = logging.StreamHandler(sys.stdout)
+    outlog.setLevel(logging.INFO)
+    log.addHandler(outlog)
+    log.setLevel(logging.INFO)
+
+    import pprint
+    pprint.pprint(get_analyses(API_KEY))
+
+    get_ocx_analysis_for_sample("PCMP_SCID00003.ST.3.2_Genomiphi_assembled.fastq", 'ocx')
